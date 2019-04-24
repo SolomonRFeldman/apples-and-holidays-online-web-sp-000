c33877863c77ac5252d48f1cbeae76c4a05286f7
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-24 12:03:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,9 +77,7 @@ def all_holidays_with_bbq(holiday_hash)
   bbq_holidays = []
   holiday_hash.each { |season, holiday|
     holiday.each { |holiday_key, supplies|
-      if supplies.include?("BBQ")
-        bbq_holidays << holiday_key
-      end
+      bbq_holidays << holiday_key if supplies.include?("BBQ")
     }
   }
   bbq_holidays
