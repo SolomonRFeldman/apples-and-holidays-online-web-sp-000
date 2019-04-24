51c852eccdc2692f41639e6a1beb431da2662bd8
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-24 11:54:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  bbq_holidays = []
+  holiday_hash.each { |season, holiday|
+    holiday.each { |holiday_key, supplies|
 end
 e = "hello everybody"
 puts e.scan(/\w+/).collect{ |value|value.capitalize}.join(" ")
